2695e291d99c663668fda63b1bc9c3bb204b7f21
kiruthika0612 <kiruthika.shanmugam006@gmail.com>
2026-04-27 09:08:40 +0530

    Update project.v

--- src/project.v
+++ src/project.v
@@ -1,23 +1,17 @@
-/*
- * Copyright (c) 2024 Your Name
- * SPDX-License-Identifier: Apache-2.0
- */
-
 `default_nettype none
 
 module tt_um_fulladder (
-    input  wire [7:0] ui_in,    // Dedicated inputs
-    output wire [7:0] uo_out,   // Dedicated outputs
-    input  wire [7:0] uio_in,   // IOs: Input path
-    output wire [7:0] uio_out,  // IOs: Output path
-    output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
-    input  wire       ena,      // always 1 when the design is powered, so you can ignore it
-    input  wire       clk,      // clock
-    input  wire       rst_n     // reset_n - low to reset
+    input  wire [7:0] ui_in,
+    output wire [7:0] uo_out,
+    input  wire [7:0] uio_in,
+    output wire [7:0] uio_out,
+    output wire [7:0] uio_oe,
+    input  wire       ena,
+    input  wire       clk,
+    input  wire       rst_n
 );
 
-  // All output pins must be assigned. If not used, assign to 0.
-     wire a   = ui_in[0];
+    wire a   = ui_in[0];
     wire b   = ui_in[1];
     wire cin = ui_in[2];
 
@@ -26,12 +20,10 @@ module tt_um_fulladder (
 
     assign {carry, sum} = a + b + cin;
 
-    assign uo_out = {6'b000000, carry, sum};
-
+    assign uo_out  = {6'b000000, carry, sum};
     assign uio_out = 8'b00000000;
     assign uio_oe  = 8'b00000000;
 
-  // List all unused inputs to prevent warnings
-  wire _unused = &{ena, clk, rst_n, 1'b0};
+    wire _unused = &{ena, clk, rst_n, 1'b0};
 
 endmodule
